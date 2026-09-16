b2b63ea66c609302ce4c9de4e2134a34db857733
Grasty01 <sambagrasty@gmail.com>
2026-09-16 21:32:01 +0000

    (fix) : dernières modifications concernant le routage de l'entité authour

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 //Point d'entrée principal qui lance le serveur
+"use strict";
+
+import authorRouter from "./src/routes/authorRoutes.js";
+import express from "express";
+
+const app = express();
+
+app.use("/api/authors", authorRouter);
+
+app.listen(3000, () => {
+  console.log("Le serveur écoute sur le port http://localhost:3000");
+});
--- backend/src/routes/authorRoutes.js
+++ backend/src/routes/authorRoutes.js
@@ -1,34 +1,32 @@
 "use strict";
 
+import {
+  getAllAuthors,
+  createAuthor,
+  updateAuthor,
+  deleteAuthor,
+} from "../controllers/authorController.js";
 import express from "express";
 const router = express.Router();
 
 /**
  * Entrée principale pour les auteurs. Affiche tous les auteurs disponibles.
  */
-router.get("/", (req, res) => {
-  res.json("Le requette a bien été routée");
-});
+router.get("/", getAllAuthors);
 
 /**
  * Création d'un auteur
  */
-router.post("/", (req, res) => {
-  res.json("Un nouvel auteur a été ajouté avec succès");
-});
+router.post("/", createAuthor);
 
 /**
  * Modification d'un auteur à partir de l'id de celui-ci
  */
-router.put("/:id", (req, res) => {
-  res.json(`L'auteur avec l'id ${req.params.id} a bien été modifié`);
-});
+router.put("/:id", updateAuthor);
 
 /**
  * Suppression d'un auteur à partir de l'id de celui-ci
  */
-router.delete("/:id", (req, res) => {
-  res.json(`L'auteur avec l'id ${req.params.id} a bien été supprimé`);
-});
+router.delete("/:id", deleteAuthor);
 
 export default router;
